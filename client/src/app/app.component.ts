02c192d854783c9e3485b28dd3191983be921e5a
tylphe <tylphet@gmail.com>
2024-01-02 09:18:04 -0700

    Angular is populating list of names from server

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'employeemanager';
+  public employees: Employee[] = [];
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  constructor(private employeeService: EmployeeService){}
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+}
--- server/src/main/java/com/tylphe/employeemanager/EmployeemanagerApplication.java
+++ server/src/main/java/com/tylphe/employeemanager/EmployeemanagerApplication.java
@@ -2,12 +2,35 @@ package com.tylphe.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeemanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeemanagerApplication.class, args);
+    }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        var corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfig.setAllowedHeaders(List.of("Origin", "Access-Control-Allow-Origin",
+                "Content-Type", "Accept", "Authorization", "Origin, Accept",
+                "X-Requested-With", "Access-Control-Request-Method",
+                "Access-Control-Request-Headers"));
+        corsConfig.setExposedHeaders(List.of("Origin", "Content-Type",
+                "Accept", "Authorization", "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        var urlSource = new UrlBasedCorsConfigurationSource();
+        urlSource.registerCorsConfiguration("/**", corsConfig);
+        return new CorsFilter(urlSource);
+    }
 }
